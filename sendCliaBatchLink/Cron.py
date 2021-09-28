fb1d884c230e087f51cc04535d87dfc1088da420
rocksnow1942 <rocksnow1942@gmail.com>
2021-09-28 14:01:38 -0700

    ON: Tue Sep 28 14:01:37 2021, FROM: Hui-PC

--- sendCliaBatchLink/Cron.py
+++ sendCliaBatchLink/Cron.py
@@ -1,9 +1,2 @@
-import sys
-sys.path.append('/home/hui/cron_python_scripts')
-from cron_helper import Logger
-import time
-import requests
-
-log = Logger(__file__ or '.')
-
-log('Run script')
+with open('/home/hui/cron_python_scripts/sendCliaBatchLink/testresult.txt','a') as f:
+    f.write(('test run'))
